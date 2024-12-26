d8dcb4c9baf4fb3e40d653cb55eabe53f3fb0a74
Felipe Santos <fcsantos.ap@gmail.com>
2024-12-26 18:56:00 +0000

    first commit

---
+++ AWSLambdaSample/AWSLambdaSample/Function.cs
@@ -0,0 +1,45 @@
+using Amazon.Lambda.Core;
+
+// Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
+[assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+
+namespace AWSLambdaSample;
+
+public class Function
+{
+    
+    /// <summary>
+    /// A simple function that takes a string and does a ToUpper
+    /// </summary>
+    /// <param name="input">The event for the Lambda function handler to process.</param>
+    /// <param name="context">The ILambdaContext that provides methods for logging and describing the Lambda environment.</param>
+    /// <returns></returns>
+    public CreateProductResponse FunctionHandler(CreateProductRequest request, ILambdaContext context)
+    {
+        //Assume Product is saved in database
+        var response = new CreateProductResponse
+        {
+            Id = Guid.NewGuid().ToString(),
+            Name = request.Name,
+            Description = request.Description,
+            Price = request.Price
+        };
+
+        return response;
+    }
+}
+
+public class CreateProductRequest
+{
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal? Price { get; set; }
+}
+
+public class CreateProductResponse
+{
+    public string? Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public decimal? Price { get; set; }
+}
